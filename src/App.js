5dd47c645322a6ce7c75ea6d9c567f402f62a703
Flavia Trotolo <ft2063@nyu.edu>
2023-04-22 21:21:43 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState({});
+
+  useEffect(() => {
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => setQuotes(data));
+  }, []);
+
+  useEffect(() => {
+    if (quotes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      setRandomQuote(quotes[randomIndex]);
+    }
+  }, [quotes]);
+
+  function handleNewQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setRandomQuote(quotes[randomIndex]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="quote-box">
+        <div className="quote-text">
+          <p>"{randomQuote.text}"</p>
+        </div>
+        <div className="quote-author">
+          <p>- {randomQuote.author || 'Unknown'}</p>
+        </div>
+        <button onClick={handleNewQuote}>New Quote</button>
+      </div>
     </div>
   );
 }
